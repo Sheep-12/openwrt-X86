c70668c0384fb82699e6b35951b326e231019449
zhanghao1989 <30239165+zhanghao1989@users.noreply.github.com>
2020-02-08 11:09:30 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,11 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+
+# 添加软件
+git clone https://github.com/KFERMercer/luci-app-serverchan package/luci-app-serverchan
+git clone https://github.com/rosywrt/luci-theme-rosy package/luci-theme-rosy
+# feeds
+./scripts/feeds update -a
+./scripts/feeds install -a
